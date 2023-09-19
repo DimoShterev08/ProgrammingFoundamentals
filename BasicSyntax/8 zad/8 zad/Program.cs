02ebb81e3de99e2baa8eac66be8bf8a51c483380
DimoShterev08 <dimo_shterev08@abv.bg>
2023-09-19 22:28:16 +0300

    re-upload homework projects

---
+++ BasicSyntax/8 zad/8 zad/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8_zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ BasicSyntax/Clock/Building/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+            for (int f = floor; f >= 1; f--)
+            {
+                for (int r = 0; r < rooms; r++)
+                {
+                    if (f==floor)
+                    {
+                        Console.Write($"L{f}{r} ");
+                    }
+                    else if (f%2==1)
+                    {
+                        Console.Write($"A{f}{r} ");
+                    }
+                    else
+                    {
+                        Console.Write($"O{f}{r} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
